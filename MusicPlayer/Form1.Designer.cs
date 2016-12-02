ba6ae6fe037b1a911b56c2c8856cf60e272483e8
nacker <648959@qq.com>
2016-12-03 02:06:26 +0800

    init

---
+++ MusicPlayer/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace MusicPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Player = new AxWMPLib.AxWindowsMediaPlayer();
+            this.addUrlBtn = new System.Windows.Forms.Button();
+            this.playBtn = new System.Windows.Forms.Button();
+            this.pauseBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.listMusics = new System.Windows.Forms.ListBox();
+            this.openBtn = new System.Windows.Forms.Button();
+            this.upBtn = new System.Windows.Forms.Button();
+            this.nextBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Player
+            // 
+            this.Player.Enabled = true;
+            this.Player.Location = new System.Drawing.Point(12, 12);
+            this.Player.Name = "Player";
+            this.Player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Player.OcxState")));
+            this.Player.Size = new System.Drawing.Size(453, 353);
+            this.Player.TabIndex = 0;
+            // 
+            // addUrlBtn
+            // 
+            this.addUrlBtn.Location = new System.Drawing.Point(653, 12);
+            this.addUrlBtn.Name = "addUrlBtn";
+            this.addUrlBtn.Size = new System.Drawing.Size(75, 23);
+            this.addUrlBtn.TabIndex = 1;
+            this.addUrlBtn.Text = "添加Url";
+            this.addUrlBtn.UseVisualStyleBackColor = true;
+            this.addUrlBtn.Click += new System.EventHandler(this.addUrlBtn_Click);
+            // 
+            // playBtn
+            // 
+            this.playBtn.Location = new System.Drawing.Point(653, 41);
+            this.playBtn.Name = "playBtn";
+            this.playBtn.Size = new System.Drawing.Size(75, 23);
+            this.playBtn.TabIndex = 2;
+            this.playBtn.Text = "播放";
+            this.playBtn.UseVisualStyleBackColor = true;
+            this.playBtn.Click += new System.EventHandler(this.playBtn_Click);
+            // 
+            // pauseBtn
+            // 
+            this.pauseBtn.Location = new System.Drawing.Point(653, 70);
+            this.pauseBtn.Name = "pauseBtn";
+            this.pauseBtn.Size = new System.Drawing.Size(75, 23);
+            this.pauseBtn.TabIndex = 3;
+            this.pauseBtn.Text = "静音";
+            this.pauseBtn.UseVisualStyleBackColor = true;
+            this.pauseBtn.Click += new System.EventHandler(this.pauseBtn_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Location = new System.Drawing.Point(653, 99);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(75, 23);
+            this.stopBtn.TabIndex = 4;
+            this.stopBtn.Text = "停止";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // listMusics
+            // 
+            this.listMusics.FormattingEnabled = true;
+            this.listMusics.ItemHeight = 12;
+            this.listMusics.Location = new System.Drawing.Point(471, 12);
+            this.listMusics.Name = "listMusics";
+            this.listMusics.Size = new System.Drawing.Size(176, 352);
+            this.listMusics.TabIndex = 5;
+            // 
+            // openBtn
+            // 
+            this.openBtn.Location = new System.Drawing.Point(654, 129);
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(75, 23);
+            this.openBtn.TabIndex = 6;
+            this.openBtn.Text = "打开";
+            this.openBtn.UseVisualStyleBackColor = true;
+            this.openBtn.Click += new System.EventHandler(this.openBtn_Click);
+            // 
+            // upBtn
+            // 
+            this.upBtn.Location = new System.Drawing.Point(654, 159);
+            this.upBtn.Name = "upBtn";
+            this.upBtn.Size = new System.Drawing.Size(75, 23);
+            this.upBtn.TabIndex = 7;
+            this.upBtn.Text = "<<";
+            this.upBtn.UseVisualStyleBackColor = true;
+            this.upBtn.Click += new System.EventHandler(this.upBtn_Click);
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.Location = new System.Drawing.Point(654, 189);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(75, 23);
+            this.nextBtn.TabIndex = 8;
+            this.nextBtn.Text = ">>";
+            this.nextBtn.UseVisualStyleBackColor = true;
+            this.nextBtn.Click += new System.EventHandler(this.nextBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 601);
+            this.Controls.Add(this.nextBtn);
+            this.Controls.Add(this.upBtn);
+            this.Controls.Add(this.openBtn);
+            this.Controls.Add(this.listMusics);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.pauseBtn);
+            this.Controls.Add(this.playBtn);
+            this.Controls.Add(this.addUrlBtn);
+            this.Controls.Add(this.Player);
+            this.Name = "Form1";
+            this.Text = "万能播放器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer Player;
+        private System.Windows.Forms.Button addUrlBtn;
+        private System.Windows.Forms.Button playBtn;
+        private System.Windows.Forms.Button pauseBtn;
+        private System.Windows.Forms.Button stopBtn;
+        private System.Windows.Forms.ListBox listMusics;
+        private System.Windows.Forms.Button openBtn;
+        private System.Windows.Forms.Button upBtn;
+        private System.Windows.Forms.Button nextBtn;
+    }
+}
+
---
+++ MusicPlayer/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MusicPlayer
+{
+    public partial class Form1 : Form
+    {
+        // 存放音乐地址数组
+        List<string> musics = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addUrlBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                Player.URL = ofd.FileName;
+                //playBtn.Enabled = true;
+                //stopBtn.Enabled = true;
+                //pauseBtn.Enabled = true;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Player.settings.autoStart = false;
+        }
+
+        private void playBtn_Click(object sender, EventArgs e)
+        {
+            //Player.Ctlcontrols.play();
+            if (playBtn.Text == "播放")
+            {
+                Player.Ctlcontrols.play();
+                playBtn.Text = "暂停";
+            }
+            else
+            {
+                Player.Ctlcontrols.pause();
+                playBtn.Text = "播放";
+            }
+        }
+
+        private void pauseBtn_Click(object sender, EventArgs e)
+        {
+            if (pauseBtn.Text == "静音")
+            {
+                Player.settings.mute = true;
+                pauseBtn.Text = "取消静音";
+            }
+            else
+            {
+                Player.settings.mute = false;
+                pauseBtn.Text = "静音";
+            }
+        }
+
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            Player.Ctlcontrols.stop();
+        }
+
+        private void openBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = @"D:\";
+            ofd.Filter = "音乐文件|*.mp3;*.wma";
+            ofd.Title = "请选择文件";
+            ofd.Multiselect = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                // listMusics.Items.AddRange(ofd.FileNames);
+                // Player.URL = listMusics.Items[1].ToString();
+
+                for (int i = 0; i < ofd.FileNames.Length; i++)
+                {
+                    musics.Add(ofd.FileNames[i]);
+                    string[] temp = ofd.FileNames[i].Split(new char[] { '\\' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    listMusics.Items.Add(temp[temp.Length - 1]);
+                }
+
+                myPlay();
+
+            }
+        }
+
+        #region 播放
+        private void myPlay(int i)
+        {
+
+            Player.URL = musics[i];
+            listMusics.SelectedIndex = i;
+            Player.Ctlcontrols.play();
+            playBtn.Text = "暂停";
+        }
+
+        private void myPlay()
+        {
+            myPlay(0);
+        }
+        #endregion
+
+        private void upBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nextBtn_Click(object sender, EventArgs e)
+        {
+            int index = listMusics.SelectedIndex + 1;
+            index++;
+            if (index > musics.Count - 1)
+            {
+                myPlay(0);
+            }
+            else
+            {
+                myPlay(index);
+            }
+           
+        }
+    }
+}
